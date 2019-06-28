b2e86c0b535933c7aea17bcea55d6c84648e4ac1
rinellam <michael@michaelrinella.com>
2019-06-27 19:00:09 -0500

    Udates to Nav
    added styling and changes to the mobile nav

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    // hamburger menu toggle
+    $('.nav-toggle').click(function() {
+        $('.main-nav').toggleClass('is-open');
+        $('.hamburger').toggleClass('is-open');
+
+    })
+
+
+
+})
